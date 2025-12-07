0a84ee27884edb2c5a8ae030e66a2dddac87485d
Harin3Bone <harin.thananam@gmail.com>
2025-12-07 15:29:29 +0700

    Refactor rectangle using setup

--- src/test/java/com/cs/ku/util/RectangleTest.java
+++ src/test/java/com/cs/ku/util/RectangleTest.java
@@ -1,44 +1,70 @@
 package com.cs.ku.util;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RectangleTest {
 
+    private Rectangle rectangle;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before all tests.");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Setup.");
+        rectangle = new Rectangle(4.0, 5.0);
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Tear down.");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After all tests.");
+    }
+
     @Test
     void testGetArea() {
-        // given
-        var rectangle = new Rectangle(5.0, 3.0);
+        System.out.println("Test getArea.");
 
         // when
         var actual = rectangle.getArea();
 
         // then
-        assertEquals(15.0, actual);
+        assertEquals(20, actual);
     }
 
     @Test
     void testGetAreaWithDelta() {
-        // given
-        var rectangle = new Rectangle(5.0, 4.9999);
+        System.out.println("Test Get Area with Delta.");
 
         // when
         var actual = rectangle.getArea();
-        assertEquals(25.0, actual, 0.01);
+
+        // then
+        assertEquals(20.0, actual, 0.01);
     }
 
     @Test
     void testChangeSize() {
-        // given
-        var rectangle = new Rectangle(2.0, 3.0);
+        System.out.println("Test Change Size.");
 
         // when
         rectangle.changeSize(10);
 
         // then
-        assertEquals(12.0, rectangle.getLength());
-        assertEquals(13.0, rectangle.getWidth());
+        assertEquals(14.0, rectangle.getLength());
+        assertEquals(15.0, rectangle.getWidth());
     }
 
 }
