43f691db602d452d26fdbcd2e4e4602ce52968f5
Harin3Bone <harin.thananam@gmail.com>
2025-12-07 14:22:24 +0700

    Impl subtract, multiple and divide

--- src/main/java/com/cs/ku/util/Calculator.java
+++ src/main/java/com/cs/ku/util/Calculator.java
@@ -6,6 +6,21 @@ public class Calculator {
         return a + b;
     }
 
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+
     public int power(int base, int exponent) {
         int result = 1;
         if (exponent < 0) return -1;
