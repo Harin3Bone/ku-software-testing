729dd2546445ee647f238538b104cc721a5be2b1
Harin3Bone <harin.thananam@gmail.com>
2026-03-24 00:34:21 +0700

    Spring - Initial Spring Boot Application

---
+++ src/main/java/com/cs/ku/KUSoftwareTestingApplication.java
@@ -0,0 +1,13 @@
+package com.cs.ku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KUSoftwareTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KUSoftwareTestingApplication.class, args);
+    }
+
+}
--- src/test/java/com/cs/ku/lecture/cucumber/CucumberSpringConfiguration.java
+++ src/test/java/com/cs/ku/lecture/cucumber/CucumberSpringConfiguration.java
@@ -1,5 +1,6 @@
 package com.cs.ku.lecture.cucumber;
 
+import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
@@ -9,9 +10,11 @@ import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
+@CucumberContextConfiguration
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
         value = "pretty, html:target/cucumber.html"
 )
-public class AppIntegrationTest {
+public class CucumberSpringConfiguration {
 }
